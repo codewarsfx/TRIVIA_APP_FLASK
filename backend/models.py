5c13cc013fee51021cd223c5e09bb4926332c291
codewarsfx <chiderainnocent001@gmail.com>
2022-06-20 13:04:23 +0100

    bug fix

--- backend/models.py
+++ backend/models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+# k
 
 database_name = 'trivia'
 database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
